ba5343d45ab043f15399af74b125a0cdfdc011aa
Orie Steele <orie@transmute.industries>
2021-03-07 10:43:39 -0600

    chore: export encoding

--- packages/web-crypto-key-pair/src/index.ts
+++ packages/web-crypto-key-pair/src/index.ts
@@ -1,6 +1,6 @@
 import * as key from './key';
-export { key };
-export * from './encoding';
+import * as encoding from './encoding';
+export { key, encoding };
 export * from './signatures';
 export * from './derive-bits';
 export * from './KeyPair';
