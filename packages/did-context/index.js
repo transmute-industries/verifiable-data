c7c67e2d11712267e47d500551f0dc4dcf2a32a0
gjgd <gjgd@protonmail.com>
2021-04-13 16:38:02 -0500

    feat: add did v1 transmute context

--- packages/did-context/index.js
+++ packages/did-context/index.js
@@ -3,5 +3,6 @@ const constants = require("./constants");
 const contexts = new Map();
 
 contexts.set(constants.DID_CONTEXT_V1_URL, require("./contexts/did-v1.json"));
+contexts.set(constants.DID_CONTEXT_TRANSMUTE_V1_URL, require("./contexts/did-v1-transmute.json"));
 
 module.exports = { constants, contexts };
