11643db12f601e37949590d6c15218aee36927db
gjgd <gjgd@protonmail.com>
2021-04-14 12:52:29 -0500

    feat: update universal wallet document loader with extended did context

--- packages/universal-wallet-test-vectors/src/documentLoader.ts
+++ packages/universal-wallet-test-vectors/src/documentLoader.ts
@@ -2,9 +2,8 @@ import {
   documentLoaderFactory,
   contexts
 } from "@transmute/jsonld-document-loader";
-
+import didContexts from '@transmute/did-context';
 import { issuers } from "./data/issuers.json";
-
 import wallet_v1 from "./data/wallet-v1.json";
 import bbs_v1 from "./data/bbs-v1.json";
 import citizenship_v1 from "./data/citizenship-v1.json";
@@ -15,11 +14,15 @@ let golem = documentLoaderFactory.pluginFactory.build({
     ...contexts.W3C_Decentralized_Identifiers,
     ...contexts.W3C_Verifiable_Credentials,
     ...contexts.W3ID_Security_Vocabulary,
-    "http://w3id.org/wallet/v1": wallet_v1,
-    "https://w3id.org/security/bbs/v1": bbs_v1,
-    "https://w3id.org/citizenship/v1": citizenship_v1,
-    "https://w3id.org/security/v3-unstable": sec_v3
-  } as any
+    [didContexts.constants
+      .DID_CONTEXT_TRANSMUTE_V1_URL]: didContexts.contexts.get(
+      didContexts.constants.DID_CONTEXT_TRANSMUTE_V1_URL,
+    ),
+    'http://w3id.org/wallet/v1': wallet_v1,
+    'https://w3id.org/security/bbs/v1': bbs_v1,
+    'https://w3id.org/citizenship/v1': citizenship_v1,
+    'https://w3id.org/security/v3-unstable': sec_v3,
+  } as any,
 });
 
 // add a resolver to each issuer... and no others.
