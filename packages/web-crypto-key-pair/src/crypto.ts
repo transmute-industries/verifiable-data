8e0675b5431ecb9fcb798117f6d45c56117986dc
gjgd <gjgd@protonmail.com>
2021-04-15 10:23:04 -0500

    feat: use @peculiar/webcrypto as a replacement for node 15 crypto module

--- packages/web-crypto-key-pair/src/crypto.ts
+++ packages/web-crypto-key-pair/src/crypto.ts
@@ -1,4 +1,8 @@
-export const subtle: SubtleCrypto =
+import { Crypto } from '@peculiar/webcrypto';
+
+const crypto = new Crypto();
+
+export const subtle: any =
   typeof window !== 'undefined' && typeof jest === 'undefined'
     ? window.crypto.subtle
-    : require('crypto').webcrypto.subtle;
+    : crypto.subtle;
