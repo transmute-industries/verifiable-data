f19535e267b99e27cf4f22683cdac5c62c2ce127
gjgd <gjgd@protonmail.com>
2021-04-15 11:06:43 -0500

    chore: update type

--- packages/web-crypto-key-pair/src/crypto.ts
+++ packages/web-crypto-key-pair/src/crypto.ts
@@ -2,7 +2,7 @@ import { Crypto } from '@peculiar/webcrypto';
 
 const crypto = new Crypto();
 
-export const subtle: any =
+export const subtle: SubtleCrypto =
   typeof window !== 'undefined' && typeof jest === 'undefined'
     ? window.crypto.subtle
     : crypto.subtle;
