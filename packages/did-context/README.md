a016ba3b6806986f2238d4ff6888e809ee637206
Orie Steele <orie@transmute.industries>
2021-05-08 10:57:55 -0500

    feat: add ld suites to did context

--- packages/did-context/README.md
+++ packages/did-context/README.md
@@ -15,3 +15,22 @@ export default {
   ),
 };
 ```
+
+##### Extension Use
+
+See [transmute extension registry](https://ns.did.ai/)
+
+```js
+const didContexts = require("@transmute/did-context");
+export default {
+  // https://www.w3.org/ns/did/v1
+  [didContexts.constants.DID_CONTEXT_V1_URL]: didContexts.contexts.get(
+    didContexts.constants.DID_CONTEXT_V1_URL
+  ),
+  // https://ns.did.ai/suites/bls12381-2020/v1
+  [didContexts.constants
+    .DID_CONTEXT_TRANSMUTE_BLS12381_2020_V1_URL]: didContexts.contexts.get(
+    didContexts.constants.DID_CONTEXT_TRANSMUTE_BLS12381_2020_V1_URL
+  ),
+};
+```
--- packages/did-context/index.js
+++ packages/did-context/index.js
@@ -3,6 +3,54 @@ const constants = require("./constants");
 const contexts = new Map();
 
 contexts.set(constants.DID_CONTEXT_V1_URL, require("./contexts/did-v1.json"));
-contexts.set(constants.DID_CONTEXT_TRANSMUTE_V1_URL, require("./contexts/did-v1-transmute.json"));
+contexts.set(
+  constants.DID_CONTEXT_TRANSMUTE_V1_URL,
+  require("./contexts/did-v1-transmute.json")
+);
+
+contexts.set(
+  constants.DID_CONTEXT_TRANSMUTE_PGP_2021_V1_URL,
+  require("./contexts/pgp-2021-v1.json")
+);
+
+contexts.set(
+  constants.DID_CONTEXT_TRANSMUTE_JWS_2020_V1_URL,
+  require("./contexts/jws-2020-v1.json")
+);
+
+contexts.set(
+  constants.DID_CONTEXT_TRANSMUTE_SECP256K1_2020_V1_URL,
+  require("./contexts/secp256k1-2020-v1.json")
+);
+
+contexts.set(
+  constants.DID_CONTEXT_TRANSMUTE_BLS12381_2020_V1_URL,
+  require("./contexts/bls12381-2020-v1.json")
+);
+
+contexts.set(
+  constants.DID_CONTEXT_TRANSMUTE_ED25519_2020_V1_URL,
+  require("./contexts/ed25519-2020-v1.json")
+);
+
+contexts.set(
+  constants.DID_CONTEXT_TRANSMUTE_X25519_2020_V1_URL,
+  require("./contexts/x25519-2020-v1.json")
+);
+
+contexts.set(
+  constants.DID_CONTEXT_TRANSMUTE_SECP256K1_2019_V1_URL,
+  require("./contexts/secp256k1-2019-v1.json")
+);
+
+contexts.set(
+  constants.DID_CONTEXT_TRANSMUTE_ED25519_2018_V1_URL,
+  require("./contexts/ed25519-2018-v1.json")
+);
+
+contexts.set(
+  constants.DID_CONTEXT_TRANSMUTE_X25519_2018_V1_URL,
+  require("./contexts/x25519-2018-v1.json")
+);
 
 module.exports = { constants, contexts };
