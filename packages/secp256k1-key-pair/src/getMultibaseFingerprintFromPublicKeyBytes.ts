360cedbf7a09b18906c4d30378a2cf37d75918e4
Orie Steele <orie@transmute.industries>
2021-05-15 13:50:56 -0500

    chore: cleanup constants

--- packages/secp256k1-key-pair/src/getMultibaseFingerprintFromPublicKeyBytes.ts
+++ packages/secp256k1-key-pair/src/getMultibaseFingerprintFromPublicKeyBytes.ts
@@ -1,14 +1,16 @@
 import { base58, base64url } from './encoding';
 
+import {
+  SECP256K1_MULTICODEC_IDENTIFIER,
+  VARIABLE_INTEGER_TRAILING_BYTE,
+} from './constants';
 export const getMultibaseFingerprintFromPublicKeyBytes = (
   publicKey: Uint8Array,
   encoding = 'base58btc'
 ): string => {
   const buffer = new Uint8Array(2 + publicKey.length);
-  // See https://github.com/multiformats/multicodec/blob/master/table.csv
-  // 0xe7 is Secp256k1 public key
-  buffer[0] = 0xe7; //
-  buffer[1] = 0x01;
+  buffer[0] = SECP256K1_MULTICODEC_IDENTIFIER;
+  buffer[1] = VARIABLE_INTEGER_TRAILING_BYTE;
   buffer.set(publicKey, 2);
   if (encoding === 'base58btc') {
     return `z${base58.encode(buffer)}`;
