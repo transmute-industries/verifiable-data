3c2744ce264e2cc6ba8b9617daa4af3c89ae7fed
Orie Steele <orie@transmute.industries>
2021-05-19 13:45:17 -0500

    chore: add suites

--- packages/security-context/constants.js
+++ packages/security-context/constants.js
@@ -1,4 +1,38 @@
 const SECURITY_CONTEXT_V1_URL = "https://w3id.org/security/v1";
 const SECURITY_CONTEXT_V2_URL = "https://w3id.org/security/v2";
 
-module.exports = { SECURITY_CONTEXT_V1_URL, SECURITY_CONTEXT_V2_URL };
+const PGP_2021_V1_URL = "https://w3id.org/security/suites/pgp-2021/v1";
+
+const JSON_WEB_SIGNATURE_2020_V1_URL =
+  "https://w3id.org/security/suites/jws-2020/v1";
+
+const ED25519_2020_V1_URL = "https://w3id.org/security/suites/ed25519-2020/v1";
+
+const X25519_2020_V1_URL = "https://w3id.org/security/suites/x25519-2020/v1";
+
+const BLS12381_2020_V1_URL =
+  "https://w3id.org/security/suites/bls12381-2020/v1";
+
+const SECP256K1_2020_V1_URL =
+  "https://w3id.org/security/suites/secp256k1-2020/v1";
+
+const SECP256k1_2019_v1_URL =
+  "https://w3id.org/security/suites/secp256k1-2019/v1";
+
+const X25519_2019_v1_URL = "https://w3id.org/security/suites/x25519-2019/v1";
+
+const ED25519_2018_v1_URL = "https://w3id.org/security/suites/ed25519-2018/v1";
+
+module.exports = {
+  SECURITY_CONTEXT_V1_URL,
+  SECURITY_CONTEXT_V2_URL,
+  PGP_2021_V1_URL,
+  JSON_WEB_SIGNATURE_2020_V1_URL,
+  ED25519_2020_V1_URL,
+  X25519_2020_V1_URL,
+  BLS12381_2020_V1_URL,
+  SECP256K1_2020_V1_URL,
+  SECP256k1_2019_v1_URL,
+  X25519_2019_v1_URL,
+  ED25519_2018_v1_URL,
+};
--- packages/security-context/index.js
+++ packages/security-context/index.js
@@ -2,14 +2,78 @@ const constants = require("./constants");
 
 const contexts = new Map();
 
+// https://w3id.org/security/v1
+
 contexts.set(
   constants.SECURITY_CONTEXT_V1_URL,
   require("./contexts/sec-v1.json")
 );
 
+// https://w3id.org/security/v2
+
 contexts.set(
   constants.SECURITY_CONTEXT_V2_URL,
   require("./contexts/sec-v2.json")
+);
+
+// https://w3id.org/security/suites/pgp-2021/v1
+
+contexts.set(constants.PGP_2021_V1_URL, require("./contexts/pgp-2021-v1.json"));
+
+// https://w3id.org/security/suites/jws-2020/v1
+
+contexts.set(
+  constants.JSON_WEB_SIGNATURE_2020_V1_URL,
+  require("./contexts/jws-2020-v1.json")
+);
+
+// https://w3id.org/security/suites/ed25519-2020/v1
+
+contexts.set(
+  constants.ED25519_2020_V1_URL,
+  require("./contexts/ed25519-2020-v1.json")
+);
+
+// https://w3id.org/security/suites/x25519-2020/v1
+
+contexts.set(
+  constants.X25519_2020_V1_URL,
+  require("./contexts/x25519-2020-v1.json")
+);
+
+// https://w3id.org/security/suites/bls12381-2020/v1
+
+contexts.set(
+  constants.BLS12381_2020_V1_URL,
+  require("./contexts/bls12381-2020-v1.json")
+);
+
+// https://w3id.org/security/suites/secp256k1-2020/v1
+
+contexts.set(
+  constants.SECP256K1_2020_V1_URL,
+  require("./contexts/secp256k1-2020-v1.json")
+);
+
+// https://w3id.org/security/suites/secp256k1-2019/v1
+
+contexts.set(
+  constants.SECP256k1_2019_v1_URL,
+  require("./contexts/secp256k1-2019-v1.json")
+);
+
+// https://w3id.org/security/suites/x25519-2019/v1
+
+contexts.set(
+  constants.X25519_2019_v1_URL,
+  require("./contexts/x25519-2019-v1.json")
+);
+
+// https://w3id.org/security/suites/ed25519-2018/v1
+
+contexts.set(
+  constants.ED25519_2018_v1_URL,
+  require("./contexts/ed25519-2018-v1.json")
 );
 
 module.exports = { constants, contexts };
