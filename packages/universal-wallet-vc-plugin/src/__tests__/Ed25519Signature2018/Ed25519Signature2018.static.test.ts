79bcc22a724cf53d0bc6e675151c9dcbcd9be9ec
Orie Steele <orie@transmute.industries>
2021-07-04 18:20:22 -0500

    chore: lint all

--- packages/universal-wallet-vc-plugin/src/__tests__/Ed25519Signature2018/Ed25519Signature2018.static.test.ts
+++ packages/universal-wallet-vc-plugin/src/__tests__/Ed25519Signature2018/Ed25519Signature2018.static.test.ts
@@ -1,6 +1,6 @@
 import {
   Ed25519Signature2018,
-  Ed25519VerificationKey2018,
+  Ed25519VerificationKey2018
 } from "@transmute/ed25519-signature-2018";
 
 import { plugin } from "../../index";
@@ -11,7 +11,7 @@ import {
   documentLoader,
   verifiableCredential,
   verifiablePresentation,
-  controller,
+  controller
 } from "./__fixtures__";
 
 let key: Ed25519VerificationKey2018;
@@ -21,7 +21,7 @@ beforeAll(async () => {
   key = await Ed25519VerificationKey2018.from(key0);
   suite = new Ed25519Signature2018({
     key,
-    date: "2021-06-19T18:53:11Z",
+    date: "2021-06-19T18:53:11Z"
   });
 });
 
@@ -31,8 +31,8 @@ describe("from / issue / present / verify", () => {
       credential: { ...credential, issuer: key.controller },
       options: {
         suite,
-        documentLoader,
-      },
+        documentLoader
+      }
     });
     expect(vc).toEqual(verifiableCredential);
   });
@@ -43,8 +43,8 @@ describe("from / issue / present / verify", () => {
         challenge: "nonce-123",
         domain: "example.com",
         suite,
-        documentLoader,
-      },
+        documentLoader
+      }
     });
     expect(vp).toEqual(verifiablePresentation);
   });
@@ -59,12 +59,12 @@ describe("from / issue / present / verify", () => {
         documentLoader: (iri: string) => {
           if (iri.startsWith(controller.id)) {
             return {
-              document: controller,
+              document: controller
             };
           }
           return documentLoader(iri);
-        },
-      },
+        }
+      }
     });
     expect(verification.verified).toBe(true);
   });
