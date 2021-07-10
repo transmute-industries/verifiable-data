303d4c0c402c11464d9b8cdf9b1e1d4c97ea964a
Orie Steele <orie@transmute.industries>
2021-07-10 16:37:37 -0500

    chore: commit ed25519 fixtures

--- packages/vc.js/src/__tests__/Ed25519Signature2018.digitalbazaar/__fixtures__/index.ts
+++ packages/vc.js/src/__tests__/Ed25519Signature2018.digitalbazaar/__fixtures__/index.ts
@@ -1,6 +1,14 @@
 import key from "./key.json";
-import doc from "./doc.json";
-import docSigned from "./doc.signed.json";
+import credential from "./credential.json";
+import verifiableCredential from "./verifiableCredential.json";
+import verifiablePresentation from "./verifiablePresentation.json";
 import controller from "./controller.json";
 import { documentLoader } from "./documentLoader";
-export { key, doc, docSigned, controller, documentLoader };
+export {
+  key,
+  credential,
+  verifiableCredential,
+  verifiablePresentation,
+  controller,
+  documentLoader,
+};
--- packages/vc.js/src/__tests__/Ed25519Signature2018.digitalbazaar/digitalbazaar.test.ts
+++ packages/vc.js/src/__tests__/Ed25519Signature2018.digitalbazaar/digitalbazaar.test.ts
@@ -1,12 +1,10 @@
 import { Ed25519VerificationKey2018 } from "@digitalbazaar/ed25519-verification-key-2018";
 import { Ed25519Signature2018 } from "@digitalbazaar/ed25519-signature-2018";
+import vc from "@digitalbazaar/vc";
 import * as fixtures from "./__fixtures__";
-import jsigs from "jsonld-signatures";
 
 let key: any;
 let suite: any;
-
-const purpose = new jsigs.purposes.AssertionProofPurpose();
 
 beforeAll(async () => {
   key = await Ed25519VerificationKey2018.from(fixtures.key);
@@ -16,26 +14,42 @@ beforeAll(async () => {
   });
 });
 
-it("sign", async () => {
-  const docSigned = await jsigs.sign(
-    { ...fixtures.doc, issuer: { id: key.controller } },
-    {
-      suite,
-      purpose,
-      documentLoader: fixtures.documentLoader,
-    }
-  );
-  expect(docSigned).toEqual(fixtures.docSigned);
+it("issue verifiable credential", async () => {
+  const docSigned = await vc.issue({
+    credential: { ...fixtures.credential, issuer: { id: key.controller } },
+    suite,
+  });
+  expect(docSigned).toEqual(fixtures.verifiableCredential);
 });
 
-it("verify", async () => {
-  const { verified } = await jsigs.verify(
-    { ...fixtures.docSigned },
-    {
-      suite: new Ed25519Signature2018(),
-      purpose: purpose,
-      documentLoader: fixtures.documentLoader,
-    }
-  );
-  expect(verified).toBe(true);
+it("verify verifiable credential", async () => {
+  const res = await vc.verifyCredential({
+    credential: fixtures.verifiableCredential,
+    suite: new Ed25519Signature2018(),
+    documentLoader: fixtures.documentLoader,
+  });
+  expect(res.verified).toBe(true);
+});
+
+it("present verifiable credential", async () => {
+  const verifiablePresentation = await vc.signPresentation({
+    presentation: {
+      "@context": fixtures.verifiableCredential["@context"],
+      type: ["VerifiablePresentation"],
+      verifiableCredential: [fixtures.verifiableCredential],
+    },
+    challenge: "123",
+    suite,
+  });
+  expect(verifiablePresentation).toEqual(fixtures.verifiablePresentation);
+});
+
+it("verify presentation", async () => {
+  const res = await vc.verify({
+    presentation: fixtures.verifiablePresentation,
+    challenge: "123",
+    suite,
+    documentLoader: fixtures.documentLoader,
+  });
+  expect(res.verified).toBe(true);
 });
