098c40ef3f49d1865d24ed822eab750f6e7d6276
Orie Steele <orie@transmute.industries>
2021-07-11 19:49:51 -0500

    chore: tests

--- packages/ed25519-signature-2018/src/__tests__/Ed25519VerificationKey2018.static.test.ts
+++ packages/ed25519-signature-2018/src/__tests__/Ed25519VerificationKey2018.static.test.ts
@@ -2,7 +2,8 @@ import {
   documentLoader,
   rawKeyJsonLd,
   credential,
-  verifiableCredential
+  verifiableCredential,
+  verifiablePresentation,
 } from "../__fixtures__";
 import * as vcjs from "@transmute/vc.js";
 import { Ed25519Signature2018, Ed25519VerificationKey2018 } from "..";
@@ -17,49 +18,52 @@ it("can from, issue, prove and verify", async () => {
         document: {
           "@context": [
             "https://www.w3.org/ns/did/v1",
-            "https://w3id.org/security/suites/ed25519-2018/v1"
+            "https://w3id.org/security/suites/ed25519-2018/v1",
           ],
           id: rawKeyJsonLd.controller,
           verificationMethod: [rawKeyJsonLd],
           authentication: [rawKeyJsonLd.id],
-          assertionMethod: [rawKeyJsonLd.id]
-        }
+          assertionMethod: [rawKeyJsonLd.id],
+        },
       };
     }
     return documentLoader(iri);
   };
 
-  const vc = await vcjs.ld.issue({
+  const vc = await vcjs.ld.createVerifiableCredential({
     credential: {
-      ...credential
+      ...credential,
     },
     suite: new Ed25519Signature2018({
       date: "2021-06-19T18:53:11Z",
-      key
+      key,
     }),
-    documentLoader: customDocumentLoader
+    documentLoader: customDocumentLoader,
   });
 
   expect(vc).toEqual(verifiableCredential);
 
-  const vp = await vcjs.ld.signPresentation({
-    presentation: await vcjs.ld.createPresentation({
-      verifiableCredential: vc,
-      holder: key.controller,
-      documentLoader: customDocumentLoader
-    }),
+  const vp = await vcjs.ld.createVerifiablePresentation({
+    presentation: {
+      "@context": ["https://www.w3.org/2018/credentials/v1"],
+      type: ["VerifiablePresentation"],
+      verifiableCredential: [vc],
+    },
     challenge: "123",
     suite: new Ed25519Signature2018({
-      key
+      key,
+      date: "2021-06-19T18:53:11Z",
     }),
-    documentLoader: customDocumentLoader
+    documentLoader: customDocumentLoader as any,
   });
 
-  const presentation = await vcjs.ld.verify({
+  expect(vp).toEqual(verifiablePresentation);
+
+  const presentation = await vcjs.ld.verifyVerifiablePresentation({
     presentation: vp,
     challenge: "123",
     suite: new Ed25519Signature2018(),
-    documentLoader: customDocumentLoader
+    documentLoader: customDocumentLoader,
   });
 
   expect(presentation.verified).toBe(true);
--- packages/ed25519-signature-2018/src/__tests__/JsonWebKey2020.static.test.ts
+++ packages/ed25519-signature-2018/src/__tests__/JsonWebKey2020.static.test.ts
@@ -2,7 +2,8 @@ import {
   documentLoader,
   rawKeyJson,
   credential,
-  verifiableCredential
+  verifiableCredential,
+  verifiablePresentation,
 } from "../__fixtures__";
 import * as vcjs from "@transmute/vc.js";
 import { Ed25519Signature2018, Ed25519VerificationKey2018 } from "..";
@@ -14,13 +15,13 @@ const customDocumentLoader = (iri: string) => {
       document: {
         "@context": [
           "https://www.w3.org/ns/did/v1",
-          "https://w3id.org/security/suites/jws-2020/v1"
+          "https://w3id.org/security/suites/jws-2020/v1",
         ],
         id: rawKeyJson.controller,
         verificationMethod: [rawKeyJson],
         authentication: [rawKeyJson.id],
-        assertionMethod: [rawKeyJson.id]
-      }
+        assertionMethod: [rawKeyJson.id],
+      },
     };
   }
   return documentLoader(iri);
@@ -29,37 +30,40 @@ const customDocumentLoader = (iri: string) => {
 it("can from, issue, prove and verify", async () => {
   const key = await Ed25519VerificationKey2018.from(rawKeyJson);
 
-  const vc = await vcjs.ld.issue({
+  const vc = await vcjs.ld.createVerifiableCredential({
     credential: {
-      ...credential
+      ...credential,
     },
     suite: new Ed25519Signature2018({
       date: "2021-06-19T18:53:11Z",
-      key
+      key,
     }),
-    documentLoader: customDocumentLoader
+    documentLoader: customDocumentLoader,
   });
 
   expect(vc).toEqual(verifiableCredential);
 
-  const vp = await vcjs.ld.signPresentation({
-    presentation: await vcjs.ld.createPresentation({
-      verifiableCredential: vc,
-      holder: key.controller,
-      documentLoader: customDocumentLoader
-    }),
+  const vp = await vcjs.ld.createVerifiablePresentation({
+    presentation: {
+      "@context": ["https://www.w3.org/2018/credentials/v1"],
+      type: ["VerifiablePresentation"],
+      verifiableCredential: [vc],
+    },
     challenge: "123",
     suite: new Ed25519Signature2018({
-      key
+      key,
+      date: "2021-06-19T18:53:11Z",
     }),
-    documentLoader: customDocumentLoader
+    documentLoader: customDocumentLoader as any,
   });
 
-  const presentation = await vcjs.ld.verify({
+  expect(vp).toEqual(verifiablePresentation);
+
+  const presentation = await vcjs.ld.verifyVerifiablePresentation({
     presentation: vp,
     challenge: "123",
     suite: new Ed25519Signature2018(),
-    documentLoader: customDocumentLoader
+    documentLoader: customDocumentLoader,
   });
 
   expect(presentation.verified).toBe(true);
