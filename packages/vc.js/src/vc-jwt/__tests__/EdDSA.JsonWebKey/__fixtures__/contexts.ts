4f621c4bd3009eb109eb17d218bf234a601f4a68
Orie Steele <orie@transmute.industries>
2021-07-18 08:43:35 -0500

    chore: fix JsonWebKey JWA header options

---
+++ packages/vc.js/src/vc-jwt/__tests__/EdDSA.JsonWebKey/__fixtures__/contexts.ts
@@ -0,0 +1,15 @@
+import cre from "@transmute/credentials-context";
+import sec from "@transmute/security-context";
+import did from "@transmute/did-context";
+
+export const contexts: any = {
+  [sec.constants.JSON_WEB_SIGNATURE_2020_V1_URL]: sec.contexts.get(
+    sec.constants.JSON_WEB_SIGNATURE_2020_V1_URL
+  ),
+  [cre.constants.CREDENTIALS_CONTEXT_V1_URL]: cre.contexts.get(
+    cre.constants.CREDENTIALS_CONTEXT_V1_URL
+  ),
+  [did.constants.DID_CONTEXT_V1_URL]: did.contexts.get(
+    did.constants.DID_CONTEXT_V1_URL
+  )
+};
