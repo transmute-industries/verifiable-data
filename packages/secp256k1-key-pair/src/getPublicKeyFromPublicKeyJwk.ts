cc8d110b5a081e31ba8e26dd1e273bd3cbbe7729
Orie Steele <orie@transmute.industries>
2021-11-06 10:06:06 -0500

    chore: fix secp256k1 lower s bug

--- packages/secp256k1-key-pair/src/getPublicKeyFromPublicKeyJwk.ts
+++ packages/secp256k1-key-pair/src/getPublicKeyFromPublicKeyJwk.ts
@@ -3,8 +3,18 @@ import secp256k1 from 'secp256k1';
 export const getPublicKeyFromPublicKeyJwk = (publicKeyJwk: any) => {
   const uncompressed = Buffer.concat([
     Buffer.from('04', 'hex'),
-    Buffer.from(publicKeyJwk.x, 'base64'),
-    Buffer.from(publicKeyJwk.y, 'base64'),
+    Buffer.from(
+      Buffer.from(publicKeyJwk.x, 'base64')
+        .toString('hex')
+        .padStart(64, '0'),
+      'hex'
+    ),
+    Buffer.from(
+      Buffer.from(publicKeyJwk.y, 'base64')
+        .toString('hex')
+        .padStart(64, '0'),
+      'hex'
+    ),
   ]);
   const compressedPublicKey = secp256k1.publicKeyConvert(
     uncompressed,
