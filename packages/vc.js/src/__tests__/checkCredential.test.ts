24f7f2caac8ca1c9433b5d290454bf233dc580d8
Adam Carnagey <adam.carnagey@transmute.industries>
2022-01-06 09:49:44 -0600

    chore(core): variable case naming fix

--- packages/vc.js/src/__tests__/checkCredential.test.ts
+++ packages/vc.js/src/__tests__/checkCredential.test.ts
@@ -1,9 +1,9 @@
 import credentialWithMS from "../__fixtures__/credentials/case-3.json";
 import credentialWithTZOffset from "../__fixtures__/credentials/case-4.json";
 import credentialWithLeapSecond from "../__fixtures__/credentials/case-5.json";
-import vcJWtWithMS from "../__fixtures__/verifiable-credentials/case-1.jwt";
-import vcJWtWithTZOffset from "../__fixtures__/verifiable-credentials/case-2.jwt";
-import vcJWtWithLeapSecond from "../__fixtures__/verifiable-credentials/case-3.jwt";
+import vcJWTWithMS from "../__fixtures__/verifiable-credentials/case-1.jwt";
+import vcJWTWithTZOffset from "../__fixtures__/verifiable-credentials/case-2.jwt";
+import vcJWTWithLeapSecond from "../__fixtures__/verifiable-credentials/case-3.jwt";
 import { checkCredential } from "../checkCredential";
 import documentLoader from "../__fixtures__/documentLoader";
 
@@ -53,7 +53,7 @@ describe("checkCredential", () => {
     it("should warn if nbf/exp loses info for ms", async () => {
       let warned = false;
       try {
-        await checkCredential(vcJWtWithMS, { documentLoader, strict: "throw" });
+        await checkCredential(vcJWTWithMS, { documentLoader, strict: "throw" });
       } catch (err) {
         warned = true;
       }
@@ -62,7 +62,7 @@ describe("checkCredential", () => {
     it("should warn if nbf/exp loses info for timezone", async () => {
       let warned = false;
       try {
-        await checkCredential(vcJWtWithTZOffset, { documentLoader, strict: "throw" });
+        await checkCredential(vcJWTWithTZOffset, { documentLoader, strict: "throw" });
       } catch (err) {
         warned = true;
       }
@@ -71,7 +71,7 @@ describe("checkCredential", () => {
     it("should warn if nbf/exp loses info for leap second", async () => {
       let warned = false;
       try {
-        await checkCredential(vcJWtWithLeapSecond, { documentLoader, strict: "throw" });
+        await checkCredential(vcJWTWithLeapSecond, { documentLoader, strict: "throw" });
       } catch (err) {
         warned = true;
       }
