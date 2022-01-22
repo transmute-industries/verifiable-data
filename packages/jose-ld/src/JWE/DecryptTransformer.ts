44bcbf0947ce76f2a411a7f9aa3b10c2e438903e
Orie Steele <orie@transmute.industries>
2022-01-22 11:54:00 -0600

    chore: cleanup

--- packages/jose-ld/src/JWE/DecryptTransformer.ts
+++ packages/jose-ld/src/JWE/DecryptTransformer.ts
@@ -5,10 +5,7 @@
 // import * as cipher from './xc20p';
 import * as cipher from './a256gcm';
 
-// import { stringToUint8Array } from './util';
-
 import * as jose from 'jose';
-// import { kekFromEphemeralPeer } from './kekFromEphemeralPeer';
 
 const CIPHER_ALGORITHMS: any = {
   [cipher.JWE_ENC]: cipher,
@@ -60,13 +57,10 @@ export class DecryptTransformer {
     if (typeof jwe.tag !== 'string') {
       throw new Error('Invalid or missing "tag".');
     }
-
     // validate encryption header
     let header;
-    // let additionalData;
     try {
       // ASCII(BASE64URL(UTF8(JWE Protected Header)))
-      // additionalData = stringToUint8Array(jwe.protected);
       header = JSON.parse(Buffer.from(jwe.protected, 'base64').toString());
     } catch (e) {
       throw new Error('Invalid JWE "protected" header.');
@@ -81,48 +75,6 @@ export class DecryptTransformer {
     if (!Array.isArray(jwe.recipients)) {
       throw new TypeError('"jwe.recipients" must be an array.');
     }
-
-    // find `keyAgreementKey` matching recipient
-    const { keyAgreementKey } = this;
-
-    const _findRecipient = (recipients: any, key: any) => {
-      return recipients.find(
-        (rec: any) =>
-          (rec.header && rec.header.kid === key.id) ||
-          rec.header.kid.split('#').pop() === key.id.split('#').pop()
-      );
-    };
-
-    const recipient = _findRecipient(jwe.recipients, keyAgreementKey);
-
-    if (!recipient) {
-      console.log(jwe.recipients, keyAgreementKey);
-      throw new Error('No matching recipient found for key agreement key.');
-    }
-    // get wrapped CEK
-    const { encrypted_key: wrappedKey } = recipient;
-    if (typeof wrappedKey !== 'string') {
-      throw new Error('Invalid or missing "encrypted_key".');
-    }
-
-    // TODO: consider a cache of encrypted_key => CEKs to reduce unwrapping
-    // calls which may even need to hit the network (e.g., Web KMS)
-
-    // derive KEK and unwrap CEK
-    // const { epk } = recipient.header;
-
-    // const { kek } = await kekFromEphemeralPeer(this.KeyPairClass)({
-    //   keyAgreementKey,
-    //   epk,
-    // });
-
-    // // console.log('kek is bad: ', kek);
-
-    // const cek = await kek.unwrapKey({ wrappedKey });
-    // if (!cek) {
-    //   // failed to unwrap key
-    //   return null;
-    // }
 
     const { privateKeyJwk } = await this.keyAgreementKey.export({
       type: 'JsonWebKey2020',
