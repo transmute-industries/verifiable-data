9e39f3502f626645c7e4e9ff8312204902312cb8
Orie Steele <orie@transmute.industries>
2022-01-22 14:23:39 -0600

    chore: prevent test thrashing

--- packages/vc.js/src/__tests__/jose-vc-jwt/generate-signatures.test.ts
+++ packages/vc.js/src/__tests__/jose-vc-jwt/generate-signatures.test.ts
@@ -9,13 +9,13 @@ const encoder = new TextEncoder();
 const privateKeys = [
   require("./__fixtures__/ES256K.privateKeyJwk.json"),
   require("./__fixtures__/EdDSA.privateKeyJwk.json"),
-  require("./__fixtures__/ES384.privateKeyJwk.json")
+  require("./__fixtures__/ES384.privateKeyJwk.json"),
 ];
 
 const crvToAlg: any = {
   Ed25519: "EdDSA",
   secp256k1: "ES256K",
-  "P-384": "ES384"
+  "P-384": "ES384",
 };
 
 const sign = async (header: any, payload: any, privateKeyJwk: any) => {
@@ -49,22 +49,23 @@ const verify = async (jws: string, publicKeyJwk: any) => {
 };
 
 describe("JWS", () => {
-  privateKeys.forEach(privateKeyJwk => {
+  privateKeys.forEach((privateKeyJwk) => {
     describe(`${privateKeyJwk.kty} ${privateKeyJwk.crv}`, () => {
       const alg = crvToAlg[privateKeyJwk.crv];
       describe(`can sign and verify with ${alg}`, () => {
         it(`simple`, async () => {
           const {
-            payload
+            payload,
           } = require("./__fixtures__/simple-string-payload.json");
           const header = { kid: "did:example:123#" + alg };
           const jws = await sign(header, payload, privateKeyJwk);
           const verified = await verify(jws, privateKeyJwk);
           expect(verified).toBe(true);
-          fs.writeFileSync(
-            path.resolve(__dirname, `./__fixtures__/${alg}.simple.signed.json`),
-            JSON.stringify({ jws }, null, 2)
-          );
+
+          // fs.writeFileSync(
+          //   path.resolve(__dirname, `./__fixtures__/${alg}.simple.signed.json`),
+          //   JSON.stringify({ jws }, null, 2)
+          // );
         });
 
         it(`complex`, async () => {
@@ -73,13 +74,13 @@ describe("JWS", () => {
           const jws = await sign(header, payload, privateKeyJwk);
           const verified = await verify(jws, privateKeyJwk);
           expect(verified).toBe(true);
-          fs.writeFileSync(
-            path.resolve(
-              __dirname,
-              `./__fixtures__/${alg}.complex.signed.json`
-            ),
-            JSON.stringify({ jws }, null, 2)
-          );
+          // fs.writeFileSync(
+          //   path.resolve(
+          //     __dirname,
+          //     `./__fixtures__/${alg}.complex.signed.json`
+          //   ),
+          //   JSON.stringify({ jws }, null, 2)
+          // );
         });
       });
     });
