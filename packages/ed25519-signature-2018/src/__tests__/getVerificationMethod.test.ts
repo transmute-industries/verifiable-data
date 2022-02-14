b324eff1fbdf90aba5e7508705a6e2533fd8c16d
Isaac Byrne <me@ipbyrne.com>
2022-02-14 11:46:18 -0600

    fix: adding test confirming DIF issue still exists

--- packages/ed25519-signature-2018/src/__tests__/getVerificationMethod.test.ts
+++ packages/ed25519-signature-2018/src/__tests__/getVerificationMethod.test.ts
@@ -2,14 +2,11 @@ import { Ed25519Signature2018 } from "../index";
 import DigitalBazzarCredential from "../__fixtures__/credentials/digital-bazaar.json";
 import TransmuteCredential from "../__fixtures__/credentials/transmute.json";
 import documentLoader from "../__fixtures__/documentLoader";
+import { purposes } from "@transmute/linked-data-proof";
 
 // IBYRNE - 02/03/2022
 // Test added for issue reported here: https://difdn.slack.com/archives/C4X50SNUX/p1643793079624299?thread_ts=1643731070.971909&cid=C4X50SNUX
-// Previously this test was failing because of the shape of our object being returned
-// from the getVerificationMethod did not match what's expected in the purpose.validate() function.
-// Now these tests should pass
-
-const purpose = {
+const mockPurpose = {
   term: 'assertionMethod',
   maxTimestampDelta: Infinity,
   validate: () => {
@@ -21,14 +18,15 @@ const purpose = {
   }
 };
 
-describe("document loader testing", () => {
+// Using a mock purpose, these credentials should verify.
+describe("Transmute and DB VCs validate with mock purpose (SHOULD PASS)", () => {
   it("DigitalBazaar credential should verify", async () => {
     const suite = new Ed25519Signature2018();
     const { proof, ...document } = DigitalBazzarCredential;
     const verifiedProof = await suite.verifyProof({
       proof,
       document: document,
-      purpose,
+      purpose: mockPurpose,
       documentLoader: documentLoader
     });
 
@@ -41,9 +39,38 @@ describe("document loader testing", () => {
     const verifiedProof = await suite.verifyProof({
       proof,
       document: document,
-      purpose,
+      purpose: mockPurpose,
       documentLoader: documentLoader
     });
     expect(verifiedProof.verified).toBe(true);
+  });
+});
+
+// Using purpose from linked-data-proof, these should NOT verify.
+// Once they are passing, the issue reported in DIF has been resolved.
+describe("Transmute and DB VCs verify with linked-data-proof purpose", () => {
+  it("DigitalBazaar credential should NOT verify", async () => {
+    const suite = new Ed25519Signature2018();
+    const { proof, ...document } = DigitalBazzarCredential;
+    const verifiedProof = await suite.verifyProof({
+      proof,
+      document: document,
+      purpose: new purposes.AssertionProofPurpose(),
+      documentLoader: documentLoader
+    });
+
+    expect(verifiedProof.verified).toBe(false);
+  });
+
+  it("Transmute credential should NOT verify", async () => {
+    const suite = new Ed25519Signature2018();
+    const { proof, ...document } = TransmuteCredential;
+    const verifiedProof = await suite.verifyProof({
+      proof,
+      document: document,
+      purpose: new purposes.AssertionProofPurpose(),
+      documentLoader: documentLoader
+    });
+    expect(verifiedProof.verified).toBe(false);
   });
 });
