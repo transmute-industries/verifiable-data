91d319501a8c6aeb23aa6606717a48d3870ed9b3
Orie Steele <orie@transmute.industries>
2022-03-13 10:51:37 -0500

    chore: wip

--- packages/jsonld-document-loader/src/index.ts
+++ packages/jsonld-document-loader/src/index.ts
@@ -7,6 +7,7 @@ export type DidPath = string;
 export type DidQuery = string;
 export type DidFragment = string;
 export type DidUrl = `${Did}${DidPath}${DidQuery}${DidFragment}`
+export type Url = `http${string}`;
 
 export type DidDocument = {
   id: Did;
@@ -23,46 +24,63 @@ export type DidResoluton = {
   didDocumentMetadata?: DidDocumentMetadata;
   didResolutionMetadata?: DidResolutionMetadata;
 };
+
+export type Context = {
+  "@context": object | string
+  [property: string]: any;
+}
+
+export type DocumentLoaderResponse = {
+  document: Context | DidDocument | any
+}
+
+
 export type DidResolver = (iri: Did) => Promise<DidResoluton>;
-type DidDereferencer = (iri: DidUrl) => Promise<any>;
+export type DidDereferencer = (iri: DidUrl) => Promise<any>;
+export type ContextResolver = (iri: string) => Promise<Context>;
+export type DocumentLoader = (iri: Did | DidUrl | string) => Promise<DocumentLoaderResponse>;
 
 
-export type StartsWithDidResolver = {
+export interface ContextMapResolver  {
+  load: ContextResolver;
+};
+
+export type ContextMap = {
+  [contextUrl: Url]: ContextResolver;
+}
+
+export interface StartsWithDidResolver  {
   resolve: DidResolver;
+};
+
+export interface ResolverMap  {
   [startsWith: Did]: DidResolver;
 };
 
-export type StartsWithDidDereferencer = {
+export interface StartsWithDidDereferencer  {
   dereference: DidDereferencer;
+};
+
+export interface DereferenceMap  {
   [startsWith: Did]: DidDereferencer;
 };
 
 
-const invokeByPrefix = (instance: any, operation: 'resolve' | 'dereference', id: Did | DidUrl)=>{
-  const startsWithKeys = Object.keys(instance).filter((k)=> k !== operation)
-    for (const startWithKey of startsWithKeys){
-      if (id.startsWith(startWithKey)){
-        return (instance as any)[startWithKey](id)
-      }
+const invokeByPrefix = (instance: any, id: Did | DidUrl | string)=>{
+
+  if (instance[id]){
+    return instance[id]
+  } 
+
+  const loaders = ['load', 'resolve', 'dereference'];
+  const startsWithKeys = Object.keys(instance).filter((k)=> !loaders.includes(k))
+  for (const startWithKey of startsWithKeys){
+    if (id.startsWith(startWithKey)){
+      return instance[startWithKey](id)
     }
-    throw new Error('Unsupported iri ' + id);
+  }
+  throw new Error('Unsupported iri ' + id);
 }
-
-export const resolutionFactoryDefault = {
-  resolve: async function (did: Did)  {
-    return invokeByPrefix(this, 'resolve', didUrlToDid(did as any))
-  },
-}
-
-export const resolverFactory = Factory.makeFactory<StartsWithDidResolver>(resolutionFactoryDefault);
-
-export const dereferenceFactoryDefault = {
-  dereference: async function (didUrl: DidUrl)  {
-    return invokeByPrefix(this, 'dereference', didUrl)
-  },
-}
-
-export const dereferencerFactory = Factory.makeFactory<StartsWithDidDereferencer>(dereferenceFactoryDefault);
 
 export const findFirstSubResourceWithId = (resource: any, id: string)=>{
   let subResource:any = null;
@@ -91,3 +109,63 @@ export const didUrlToDid = (didUrl: DidUrl): Did =>{
   const { method, idchar} = match?.groups
   return `did:${method}:${idchar}` as Did
 }
+
+export const resolutionFactoryDefault = {
+  resolve: async function (did: Did)  {
+    return invokeByPrefix(this, didUrlToDid(did as any))
+  },
+}
+
+export interface DidResolverFactory extends ResolverMap, StartsWithDidResolver {}
+export const resolverFactory = Factory.makeFactory<DidResolverFactory>(resolutionFactoryDefault);
+
+export const dereferenceFactoryDefault = {
+  dereference: async function (didUrl: DidUrl)  {
+    return invokeByPrefix(this,  didUrl)
+  },
+}
+
+export interface DidDereferencerFactory extends DereferenceMap, StartsWithDidDereferencer {}
+export const dereferencerFactory = Factory.makeFactory<DidDereferencerFactory>(dereferenceFactoryDefault);
+
+export const contextFactoryDefault = {
+  load: async function (iri: string): Promise<Context>  {
+    return invokeByPrefix(this,  iri)
+  },
+}
+
+export interface ContextFactory extends ContextMap, ContextMapResolver {}
+export const contextFactory = Factory.makeFactory<ContextFactory>(contextFactoryDefault);
+
+export interface DocumentLoaderFactory extends 
+ContextMapResolver, 
+StartsWithDidResolver, 
+StartsWithDidDereferencer {}
+
+const iriToLoader = (iri:string): 'load' | 'resolve' | 'dereference' => {
+  if (iri.startsWith('http')){
+    return 'load'
+  }
+  // we don't use resolve any more, dereference is a super set. 
+  if (iri.startsWith('did')){
+    return 'dereference' 
+  }
+  throw new Error('Unsupported iri: ' + iri)
+}
+
+const internalDocumentLoaderFactory:any = Factory.makeFactory<DocumentLoaderFactory>({
+  ...contextFactoryDefault, 
+  ...resolutionFactoryDefault, 
+  ...dereferenceFactoryDefault
+});
+const originalBuilder = internalDocumentLoaderFactory.build;
+// this time, make the builder return a documentLoader that is ready for use.
+internalDocumentLoaderFactory.build = function (args: ContextMap | ResolverMap | DereferenceMap ): DocumentLoader {
+  const internal = originalBuilder(args);
+  return async (iri:string) => {
+    const loader = iriToLoader(iri)
+    const document = await internal[loader](iri)
+    return { document }
+  }
+}
+export const documentLoaderFactory = internalDocumentLoaderFactory
