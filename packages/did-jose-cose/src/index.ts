c0f14d0909b0ada499a8c2544d2320e1e3404f64
Orie Steele <orie@transmute.industries>
2022-03-19 09:31:38 -0500

    chore: move cose temp

--- packages/did-jose-cose/src/index.ts
+++ packages/did-jose-cose/src/index.ts
@@ -1,5 +1,5 @@
-import * as cose from "./cose";
+// import * as cose from "./cose";
 import * as jose from "./jose";
 export * from "./types";
-export { cose, jose };
-export default { cose, jose };
+export { jose };
+export default { jose };
