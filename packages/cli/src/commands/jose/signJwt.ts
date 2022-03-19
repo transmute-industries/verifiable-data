8ce4f4c9d0261f3e1f98a86ee20a73f470550611
Orie Steele <orie@transmute.industries>
2022-03-19 12:55:46 -0500

    chore: updates

--- packages/cli/src/commands/jose/signJwt.ts
+++ packages/cli/src/commands/jose/signJwt.ts
@@ -7,6 +7,30 @@ import {
 
 import { deriveKey } from '../key';
 
+export type IssuanceMethod =
+  | 'from-path-to-json-web-key'
+  | 'from-json-web-key'
+  | 'from-vc-api'
+  | 'from-mnemonic';
+
+export const argvToIssuanceMethod = (argv: any): IssuanceMethod => {
+  if (argv.key) {
+    try {
+      JSON.parse(argv.key);
+      return 'from-json-web-key';
+    } catch (e) {
+      return 'from-path-to-json-web-key';
+    }
+  }
+  if (argv.endpoint) {
+    return 'from-vc-api';
+  }
+  if (argv.mnemonic) {
+    return 'from-mnemonic';
+  }
+  throw new Error('Cannot determine issuance method');
+};
+
 export const sign = (
   header: Header,
   payload: Payload,
@@ -15,24 +39,31 @@ export const sign = (
   return jose.jwt.sign(header, payload, privateKeyJwk);
 };
 
+export const getKey = async (argv: any) => {
+  let key: any = null;
+  const issuanceMethod = argvToIssuanceMethod(argv);
+  if (issuanceMethod === 'from-json-web-key') {
+    key = JSON.parse(argv.key);
+  }
+  if (issuanceMethod === 'from-path-to-json-web-key') {
+    key = getKeyFromFile(argv.key);
+  }
+  if (issuanceMethod === 'from-mnemonic') {
+    const keys = await deriveKey(argv.type, argv.mnemonic, argv.hdpath);
+    key = keys[0];
+  }
+  return key;
+};
+
 export const signJwt = async (argv: any) => {
   if (argv.debug) {
     console.log(argv);
   }
-  let key;
   let payload = getPayloadFromFile(argv.input);
   let data: any = {};
-  if (!argv.endpoint) {
-    if (argv.key) {
-      key = getKeyFromFile(argv.key);
-    }
-    if (argv.mnemonic) {
-      const keys = await deriveKey(argv.type, argv.mnemonic, argv.hdpath);
-      key = keys[0];
-    }
-    const kid = argv.kid;
-    data.jwt = await sign({ kid }, payload, key.privateKeyJwk);
-  }
+  const key = await getKey(argv);
+  const kid = argv.kid || key.id;
+  data.jwt = await sign({ kid }, payload, key.privateKeyJwk);
 
   handleCommandResponse(argv, data, argv.output);
 };
--- packages/cli/src/util/documentLoader.ts
+++ packages/cli/src/util/documentLoader.ts
@@ -1,10 +1,20 @@
 import { contexts } from './contexts';
 import axios from 'axios';
-import { findFirstSubResourceWithId } from '@transmute/jsonld-document-loader';
-
+import {
+  didUrlToDid,
+  findFirstSubResourceWithId,
+} from '@transmute/jsonld-document-loader';
+import { resolve } from '@transmute/did-key.js';
 export const documentLoader = async (iri: string): Promise<any> => {
   if (contexts[iri]) {
     return { document: contexts[iri] };
+  }
+  if (iri.startsWith('did:key:')) {
+    const did = didUrlToDid(iri as any);
+    const { didDocument } = await resolve(did, {
+      accept: 'application/did+json',
+    });
+    return { document: findFirstSubResourceWithId(didDocument, iri as any) };
   }
   if (iri.startsWith('did:web:')) {
     const config: any = {
