a70aaa16af5a34993346ae94a1256aecccb7fa6f
Isaac Byrne <me@ipbyrne.com>
2022-07-21 12:17:37 -0500

    fix: seperating proof and revocation errors

--- packages/vc.js/src/vc-ld/__tests__/revocation-list-2020/__fixtures__/documentLoader.ts
+++ packages/vc.js/src/vc-ld/__tests__/revocation-list-2020/__fixtures__/documentLoader.ts
@@ -1,4 +1,4 @@
-import { contexts } from "./contexts";
+import { contexts, revocationContexts } from "./contexts";
 import controller from "./controller.json";
 import controller2 from "./controller2.json";
 import controller3 from "./controller3.json";
@@ -8,6 +8,91 @@ import signedRevocationList2020 from "./signed-revocation-list-2020.json";
 
 import revocationListCredential from "./revocationListCredential.json";
 const contextResolver = async (iri: string) => {
+  if (revocationContexts[iri]) {
+    return { document: contexts[iri] };
+  }
+
+  if (iri === 'did:key:z6MkfJSbaRjStEZWd8KiV2zUSfnEXnFX5iviaLe1ArMTkNXF#z6MkfJSbaRjStEZWd8KiV2zUSfnEXnFX5iviaLe1ArMTkNXF') {
+    return {
+      document: {
+        "@context": [
+          "https://www.w3.org/ns/did/v1",
+          "https://w3id.org/security/suites/jws-2020/v1"
+        ],
+        "id": "did:key:z6MkfJSbaRjStEZWd8KiV2zUSfnEXnFX5iviaLe1ArMTkNXF",
+        "verificationMethod": [
+          {
+            "id": "did:key:z6MkfJSbaRjStEZWd8KiV2zUSfnEXnFX5iviaLe1ArMTkNXF#z6MkfJSbaRjStEZWd8KiV2zUSfnEXnFX5iviaLe1ArMTkNXF",
+            "type": "JsonWebKey2020",
+            "controller": "did:key:z6MkfJSbaRjStEZWd8KiV2zUSfnEXnFX5iviaLe1ArMTkNXF",
+            "publicKeyJwk": {
+              "kty": "OKP",
+              "crv": "Ed25519",
+              "x": "DJlj5uNt8eX7LVaabGlSYdzrXjhzjmrlSY35RNd9GmY"
+            }
+          },
+          {
+            "id": "did:key:z6MkfJSbaRjStEZWd8KiV2zUSfnEXnFX5iviaLe1ArMTkNXF#z6LShh513ABHh6V4JhAjuhcQCXGethoWrxcWRgfDfbhkJZfS",
+            "type": "JsonWebKey2020",
+            "controller": "did:key:z6MkfJSbaRjStEZWd8KiV2zUSfnEXnFX5iviaLe1ArMTkNXF",
+            "publicKeyJwk": {
+              "kty": "OKP",
+              "crv": "X25519",
+              "x": "WWC7tPmiV4NK5Z0NDgKuqJCGWxo2yb6GBjk2uL60dA0"
+            }
+          }
+        ],
+        "assertionMethod": [
+          "did:key:z6MkfJSbaRjStEZWd8KiV2zUSfnEXnFX5iviaLe1ArMTkNXF#z6MkfJSbaRjStEZWd8KiV2zUSfnEXnFX5iviaLe1ArMTkNXF"
+        ],
+        "authentication": [
+          "did:key:z6MkfJSbaRjStEZWd8KiV2zUSfnEXnFX5iviaLe1ArMTkNXF#z6MkfJSbaRjStEZWd8KiV2zUSfnEXnFX5iviaLe1ArMTkNXF"
+        ],
+        "capabilityInvocation": [
+          "did:key:z6MkfJSbaRjStEZWd8KiV2zUSfnEXnFX5iviaLe1ArMTkNXF#z6MkfJSbaRjStEZWd8KiV2zUSfnEXnFX5iviaLe1ArMTkNXF"
+        ],
+        "capabilityDelegation": [
+          "did:key:z6MkfJSbaRjStEZWd8KiV2zUSfnEXnFX5iviaLe1ArMTkNXF#z6MkfJSbaRjStEZWd8KiV2zUSfnEXnFX5iviaLe1ArMTkNXF"
+        ],
+        "keyAgreement": [
+          "did:key:z6MkfJSbaRjStEZWd8KiV2zUSfnEXnFX5iviaLe1ArMTkNXF#z6LShh513ABHh6V4JhAjuhcQCXGethoWrxcWRgfDfbhkJZfS"
+        ]
+      }
+    }
+  }
+
+  if (iri === 'http://localhost:8080/credentials/489ccb71-9352-44f8-ba4e-a8b6f03c44a0') {
+    return {
+      document: {
+        "@context": [
+          "https://www.w3.org/2018/credentials/v1",
+          "https://w3id.org/vc-revocation-list-2020/v1"
+        ],
+        "id": "http://localhost:8080/credentials/489ccb71-9352-44f8-ba4e-a8b6f03c44a0",
+        "type": [
+          "VerifiableCredential",
+          "RevocationList2020Credential"
+        ],
+        "credentialSubject": {
+          "id": "http://localhost:8080/credentials/489ccb71-9352-44f8-ba4e-a8b6f03c44a0#list",
+          "type": "RevocationList2020",
+          "encodedList": "H4sIAAAAAAAAA-3BIQEAAAACIP1_2hcmoAEAAAAAAAAAAAAAAPgbF0EzbtQwAAA"
+        },
+        "issuer": {
+          "id": "did:key:z6MkfJSbaRjStEZWd8KiV2zUSfnEXnFX5iviaLe1ArMTkNXF"
+        },
+        "issuanceDate": "2022-07-21T15:46:56.274Z",
+        "proof": {
+          "type": "Ed25519Signature2018",
+          "created": "2022-07-21T16:58:08Z",
+          "verificationMethod": "did:key:z6MkfJSbaRjStEZWd8KiV2zUSfnEXnFX5iviaLe1ArMTkNXF#z6MkfJSbaRjStEZWd8KiV2zUSfnEXnFX5iviaLe1ArMTkNXF",
+          "proofPurpose": "assertionMethod",
+          "jws": "eyJhbGciOiJFZERTQSIsImI2NCI6ZmFsc2UsImNyaXQiOlsiYjY0Il19..QzqJzzT4UFSfKJRDC22c6kRAg-H56dRQkJLIro0JBpWXhly1WaG4DVYqOtXN04VzJHFoyeC-nJgioOJmpApoDA"
+        }
+      }
+    }
+  }
+
   if (contexts[iri]) {
     return { document: contexts[iri] };
   }
