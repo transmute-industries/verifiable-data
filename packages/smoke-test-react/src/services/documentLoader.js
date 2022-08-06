7543b6499c4ced59ec21a16db0cf755f7f8b2ef4
Orie Steele <orie@transmute.industries>
2022-08-06 10:15:30 -0500

    remove cruft

--- packages/smoke-test-react/src/services/documentLoader.js
+++
@@ -1,9 +0,0 @@
-import { contexts } from "./contexts";
-export const documentLoader = (iri) => {
-  if (contexts[iri]) {
-    return { document: contexts[iri] };
-  }
-  const message = "Unsupported iri: " + iri;
-  console.error(message);
-  throw new Error(message);
-};
