d0b7c47e1849c9d688c937d41f2833af0d35febd
Orie Steele <orie@transmute.industries>
2022-12-18 17:00:00 -0600

    chore: lint

--- packages/merkle-proof/src/v2/preview.ts
+++ packages/merkle-proof/src/v2/preview.ts
@@ -1,24 +1,23 @@
-
 type AutographNode = {
-  id: string,
-  label?: string
-}
+  id: string;
+  label?: string;
+};
 
 type AutographEdge = {
-  source: string,
-  target: string,
-  label?: string
-}
+  source: string;
+  target: string;
+  label?: string;
+};
 
 type Autograph = {
-  title?: string,
-  nodes: AutographNode[]
-  links: AutographEdge[]
-}
+  title?: string;
+  nodes: AutographNode[];
+  links: AutographEdge[];
+};
 
 type AutographOptions = {
-  style?: 'subtle'
-}
+  style?: "subtle";
+};
 
 const transmute = {
   primary: {
@@ -26,83 +25,100 @@ const transmute = {
     red: "#ff605d",
     orange: "#fcb373",
     grey: "#f5f7fd",
-    white: "#fff",
+    white: "#fff"
   },
   secondary: {
     teal: "#48caca",
     aqua: "#2cb3d9",
     dark: "#2a2d4c",
     medium: "#565a7c",
-    light: "#8286a3",
-  },
+    light: "#8286a3"
+  }
 };
 
-const addNode = (node:AutographNode)=>{
-  let shape = `(${node.label || node.id})`
-  return `\t\t${node.id}${shape} \n`
-}
+const addNode = (node: AutographNode) => {
+  let shape = `(${node.label || node.id})`;
+  return `\t\t${node.id}${shape} \n`;
+};
 
-const addEdge = (link:AutographEdge, options:AutographOptions)=>{
-  const target = `${link.target}`
-  const linkStyle = link.label ? `-- ${link.label} -->`: options.style === 'subtle' ?  `-->`: `-.->`
-  return `\t\t${link.source} ${linkStyle} ${target} \n`
-}
+const addEdge = (link: AutographEdge, options: AutographOptions) => {
+  const target = `${link.target}`;
+  const linkStyle = link.label
+    ? `-- ${link.label} -->`
+    : options.style === "subtle"
+    ? `-->`
+    : `-.->`;
+  return `\t\t${link.source} ${linkStyle} ${target} \n`;
+};
 
-const isRoot = (autograph:Autograph, node:AutographNode)=>{
-  return 0 == autograph.links.filter((link:AutographEdge)=>{
-    return link.source === node.id
-  }).length;
-}
+const isRoot = (autograph: Autograph, node: AutographNode) => {
+  return (
+    0 ===
+    autograph.links.filter((link: AutographEdge) => {
+      return link.source === node.id;
+    }).length
+  );
+};
 
-const isLeaf = (autograph:Autograph, node:AutographNode)=>{
-  return 0 == autograph.links.filter((link:AutographEdge)=>{
-    return link.target === node.id
-  }).length;
-}
+const isLeaf = (autograph: Autograph, node: AutographNode) => {
+  return (
+    0 ===
+    autograph.links.filter((link: AutographEdge) => {
+      return link.target === node.id;
+    }).length
+  );
+};
 
-const isNodeInLabeledPath = (autograph:Autograph, node:AutographNode)=>{
+const isNodeInLabeledPath = (autograph: Autograph, node: AutographNode) => {
   let flag = false;
-  autograph.links.forEach((link:AutographEdge)=>{
-    if (link.label && (link.source === node.id || link.target === node.id)){
+  autograph.links.forEach((link: AutographEdge) => {
+    if (link.label && (link.source === node.id || link.target === node.id)) {
       flag = true;
     }
-  })
+  });
   return flag;
-}
+};
 
-const transmuteLinkStyle = (link:AutographEdge, index: number): string =>{
-  if (['left', 'right'].includes(link.label || 'none') ){
-    return `linkStyle ${index} color:${transmute.primary.red}, stroke-width: 2.0px` 
-  } else if (link.label){
-    return `linkStyle ${index} color:${transmute.primary.orange}, stroke-width: 2.0px`
-  } 
-  return `linkStyle ${index} color:${transmute.secondary.medium}, stroke:${transmute.secondary.medium}, stroke-width: 1.0px`
-}
+const transmuteLinkStyle = (link: AutographEdge, index: number): string => {
+  if (["left", "right"].includes(link.label || "none")) {
+    return `linkStyle ${index} color:${transmute.primary.red}, stroke-width: 2.0px`;
+  } else if (link.label) {
+    return `linkStyle ${index} color:${transmute.primary.orange}, stroke-width: 2.0px`;
+  }
+  return `linkStyle ${index} color:${transmute.secondary.medium}, stroke:${transmute.secondary.medium}, stroke-width: 1.0px`;
+};
 
-const transmuteNodeStyle = (autograph: Autograph, node:AutographNode, options:AutographOptions) => {
-  if (options.style !== 'subtle'){
-    if (isRoot(autograph, node)){
-      return `style ${node.id} color:${transmute.primary.grey}, fill:${transmute.primary.purple.light}, stroke:${transmute.primary.purple.dark}, stroke-width: 2.0px`
-    } else if (isLeaf(autograph, node) && node.label){
-      return `style ${node.id} color:${transmute.primary.grey}, fill:${transmute.primary.purple.light}, stroke:${transmute.primary.purple.dark}, stroke-width: 2.0px`
-    } else if (isNodeInLabeledPath(autograph, node)){
-      return `style ${node.id} color:${transmute.secondary.light}, stroke:${transmute.secondary.light}, stroke-width: 1.0px`
+const transmuteNodeStyle = (
+  autograph: Autograph,
+  node: AutographNode,
+  options: AutographOptions
+) => {
+  if (options.style !== "subtle") {
+    if (isRoot(autograph, node)) {
+      return `style ${node.id} color:${transmute.primary.grey}, fill:${transmute.primary.purple.light}, stroke:${transmute.primary.purple.dark}, stroke-width: 2.0px`;
+    } else if (isLeaf(autograph, node) && node.label) {
+      return `style ${node.id} color:${transmute.primary.grey}, fill:${transmute.primary.purple.light}, stroke:${transmute.primary.purple.dark}, stroke-width: 2.0px`;
+    } else if (isNodeInLabeledPath(autograph, node)) {
+      return `style ${node.id} color:${transmute.secondary.light}, stroke:${transmute.secondary.light}, stroke-width: 1.0px`;
     }
   }
-  return `style ${node.id} stroke:${transmute.secondary.medium}, stroke-width: 1.0px`
-}
+  return `style ${node.id} stroke:${transmute.secondary.medium}, stroke-width: 1.0px`;
+};
 
-const autographToMermaid = (autograph:Autograph, options:AutographOptions = {}) => {
-  let final = ''
-  autograph.nodes.forEach((node:AutographNode) => {
-    final += addNode(node)
-    final += `\t\t${transmuteNodeStyle(autograph, node, options)} \n`
+const autographToMermaid = (
+  autograph: Autograph,
+  options: AutographOptions = {}
+) => {
+  let final = "";
+  autograph.nodes.forEach((node: AutographNode) => {
+    final += addNode(node);
+    final += `\t\t${transmuteNodeStyle(autograph, node, options)} \n`;
   });
-  autograph.links.forEach((link:AutographEdge, index: number) => {
-    final += addEdge(link, options)
-    final += `\t\t${transmuteLinkStyle(link, index)} \n`
+  autograph.links.forEach((link: AutographEdge, index: number) => {
+    final += addEdge(link, options);
+    final += `\t\t${transmuteLinkStyle(link, index)} \n`;
   });
-  final = final.substring(0, final.length-1)
+  final = final.substring(0, final.length - 1);
   return `
 \`\`\`mermaid
 %%{
@@ -123,13 +139,15 @@ const autographToMermaid = (autograph:Autograph, options:AutographOptions = {}) 
   }
 }%%
 graph
-linkStyle default fill:none, stroke-width: 1px, stroke: ${transmute.secondary.medium}
-  subgraph ${autograph.title || '&nbsp;'}
+linkStyle default fill:none, stroke-width: 1px, stroke: ${
+    transmute.secondary.medium
+  }
+  subgraph ${autograph.title || "&nbsp;"}
     direction LR
 ${final}
   end
 \`\`\`
-` 
+`;
 };
 
-export default autographToMermaid
+export default autographToMermaid;
--- packages/merkle-proof/src/v2/v2.test.ts
+++ packages/merkle-proof/src/v2/v2.test.ts
@@ -1,9 +1,9 @@
-import { 
-  computeTree, 
-  generateProof, 
-  validateProof, 
+import {
+  computeTree,
+  generateProof,
+  validateProof,
   encodeProofAsObject,
-  encodeTreeAsUrn, 
+  encodeTreeAsUrn,
   encodeTreeAsObject,
   encodeProofAsUrn,
   addProofToTree,
@@ -15,77 +15,101 @@ import {
 
 import preview from "./preview";
 
-import fs from 'fs'
+import fs from "fs";
 
-const members: Buffer[] = Array.from({ length: 8 }, (_x, i) => Buffer.from(i.toString()));
-const member: Buffer = members[3]
+const members: Buffer[] = Array.from({ length: 8 }, (_x, i) =>
+  Buffer.from(i.toString())
+);
+const member: Buffer = members[3];
 const tree = computeTree(members);
-const root = tree[0][0]
+const root = tree[0][0];
 
 it("preview tree", () => {
-  const autograph = encodeTreeAsObject(tree)
-  fs.writeFileSync('./src/v2/examples/merkle-tree.mermaid.md', preview(autograph, { style: 'subtle' }))
-  const treeUrn = encodeTreeAsUrn(tree)
-  fs.writeFileSync('./src/v2/examples/merkle-tree.urn.md', `\`\`\`url\n${treeUrn}\n\`\`\``)
-  const treeJson = treeUrnToJson(treeUrn)
-  fs.writeFileSync('./src/v2/examples/merkle-tree.urn.json', treeJson)
+  const autograph = encodeTreeAsObject(tree);
+  fs.writeFileSync(
+    "./src/v2/examples/merkle-tree.mermaid.md",
+    preview(autograph, { style: "subtle" })
+  );
+  const treeUrn = encodeTreeAsUrn(tree);
+  fs.writeFileSync(
+    "./src/v2/examples/merkle-tree.urn.md",
+    `\`\`\`url\n${treeUrn}\n\`\`\``
+  );
+  const treeJson = treeUrnToJson(treeUrn);
+  fs.writeFileSync("./src/v2/examples/merkle-tree.urn.json", treeJson);
 });
 
-
-describe('proof',()=>{
+describe("proof", () => {
   it("validateProof", () => {
     const proof = generateProof(member, tree);
-    expect(validateProof(member, proof, root)).toBe(true)
+    expect(validateProof(member, proof, root)).toBe(true);
   });
 
   it("encodeProofAsUrn", () => {
     const proof = generateProof(member, tree);
-    const proofUrn = encodeProofAsUrn(root, proof, member)
-    expect(validateProofUrn(proofUrn, member)).toBe(true)
-    fs.writeFileSync('./src/v2/examples/merkle-proof.urn.md', `\`\`\`url\n${proofUrn}\n\`\`\``)
+    const proofUrn = encodeProofAsUrn(root, proof, member);
+    expect(validateProofUrn(proofUrn, member)).toBe(true);
+    fs.writeFileSync(
+      "./src/v2/examples/merkle-proof.urn.md",
+      `\`\`\`url\n${proofUrn}\n\`\`\``
+    );
   });
 
   it("proofUrnToJson", () => {
     const proof = generateProof(member, tree);
-    const proofUrn = encodeProofAsUrn(root, proof, member)
-    const proofJson = proofUrnToJson(proofUrn)
-    fs.writeFileSync('./src/v2/examples/merkle-proof.urn.json', proofJson)
+    const proofUrn = encodeProofAsUrn(root, proof, member);
+    const proofJson = proofUrnToJson(proofUrn);
+    fs.writeFileSync("./src/v2/examples/merkle-proof.urn.json", proofJson);
   });
 
   it("mermaid", () => {
     const proof = generateProof(member, tree);
     const autograph = encodeProofAsObject(root, proof, member);
-    fs.writeFileSync('./src/v2/examples/merkle-proof.mermaid.md', preview(autograph))
+    fs.writeFileSync(
+      "./src/v2/examples/merkle-proof.mermaid.md",
+      preview(autograph)
+    );
   });
-})
+});
 
 it("preview tree & proof in mermaid", () => {
-  const treeGraph = encodeTreeAsObject(tree)
+  const treeGraph = encodeTreeAsObject(tree);
   const proof = generateProof(member, tree);
-  const proofGraph = encodeProofAsObject(root, proof, member)
-  const combinedGraph = addProofToTree(treeGraph, proofGraph)
-  fs.writeFileSync('./src/v2/examples/merkle-tree-with-proof.mermaid.md', preview(combinedGraph))
+  const proofGraph = encodeProofAsObject(root, proof, member);
+  const combinedGraph = addProofToTree(treeGraph, proofGraph);
+  fs.writeFileSync(
+    "./src/v2/examples/merkle-tree-with-proof.mermaid.md",
+    preview(combinedGraph)
+  );
 });
 
 it("nicer api", () => {
-  const merkleTree = MerkleTree.from(members)
-  const proofUrn = merkleTree.getProofUrn(member)
-  const valid = MerkleTree.validateProofUrn(proofUrn, member)
+  const merkleTree = MerkleTree.from(members);
+  const proofUrn = merkleTree.getProofUrn(member);
+  const valid = MerkleTree.validateProofUrn(proofUrn, member);
   expect(valid).toBe(true);
   const g0 = MerkleTree.autograph.raw.tree(members);
-  fs.writeFileSync('./src/v2/examples/merkle-tree.autograph.json', JSON.stringify(g0, null, 2))
+  fs.writeFileSync(
+    "./src/v2/examples/merkle-tree.autograph.json",
+    JSON.stringify(g0, null, 2)
+  );
 
   const proof = generateProof(member, tree);
   const g1 = MerkleTree.autograph.raw.proof(root, proof, member);
-  fs.writeFileSync('./src/v2/examples/merkle-proof.autograph.json', JSON.stringify(g1, null, 2))
+  fs.writeFileSync(
+    "./src/v2/examples/merkle-proof.autograph.json",
+    JSON.stringify(g1, null, 2)
+  );
 
   const g2 = MerkleTree.autograph.urn.tree(merkleTree.urn);
-  fs.writeFileSync('./src/v2/examples/merkle-tree.autograph.g2.json', JSON.stringify(g2, null, 2))
+  fs.writeFileSync(
+    "./src/v2/examples/merkle-tree.autograph.g2.json",
+    JSON.stringify(g2, null, 2)
+  );
 
   const g3 = MerkleTree.autograph.urn.proof(proofUrn);
-  fs.writeFileSync('./src/v2/examples/merkle-proof.autograph.g3.json', JSON.stringify(g3, null, 2))
-
+  fs.writeFileSync(
+    "./src/v2/examples/merkle-proof.autograph.g3.json",
+    JSON.stringify(g3, null, 2)
+  );
 });
-
-
-
